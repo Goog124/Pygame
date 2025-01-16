3ca4c13a0b2cce7aa6ab117703e59cd2ec37f5cd
Константин <Kza4@yandex.ru>
2025-01-16 18:26:39 +0300

    mountains2

--- monuntain.py
+++ monuntain.py
@@ -10,6 +10,7 @@ def load_image(name):
     image = pygame.image.load(fullname)
     return image
 
+
 class Mountain(pygame.sprite.Sprite):
     image = load_image("mountains.png")
 
@@ -23,18 +24,34 @@ class Mountain(pygame.sprite.Sprite):
         self.rect.bottom = height
 
 
+class Landing(pygame.sprite.Sprite):
+    image = load_image("pt.png")
+
+    def __init__(self, pos):
+        super().__init__(all_sprites)
+        self.image = Landing.image
+        self.rect = self.image.get_rect()
+        # вычисляем маску для эффективного сравнения
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self):
+        self.rect = self.rect.move(0, 1)
+
 if __name__ == '__main__':
     pygame.init()
-    size = width, height = 500, 500
+    size = width, height = 789, 500
     screen = pygame.display.set_mode(size)
     running = True
     all_sprites = pygame.sprite.Group()
+    mountain = Mountain()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                all_sprites.update(event.pos)
+                Landing(event.pos)
         screen.fill((255, 255, 255))
         all_sprites.draw(screen)
         pygame.display.flip()
